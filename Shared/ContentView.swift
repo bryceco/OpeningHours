fc947574cb2c132bee4eeac529a2dd8e8d6aca29
Bryce Cogswell <bryceco@yahoo.com>
2021-03-03 16:05:36 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Bryce Cogswell on 3/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/OpeningHoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningHoursApp.swift
+//  Shared
+//
+//  Created by Bryce Cogswell on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct OpeningHoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
